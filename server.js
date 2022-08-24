a6fe880ced43037ba15ffa4558f9739fc179b055
jbep <joshuabemartin@gmail.com>
2022-08-23 20:23:46 -0400

    fixed server.js and created controllers

--- server.js
+++ server.js
@@ -2,14 +2,19 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override');
 const PORT = 3000
 // const PORT = process.env.PORT
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+const booksController = require('./controllers/index.js')
+
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
+
+app.use('/', booksController)
 
 app.listen(PORT, () => {
   console.log(`server listening at port ${PORT} ✨`)
