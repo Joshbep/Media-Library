49c43ddd990ac3c4a9939b71684bc778a92d300e
jbep <joshuabemartin@gmail.com>
2022-08-30 00:55:29 -0400

    finished styling for background color

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/books.js')
+const Movie = require('../models/movies.js')
 
 router.get('/', async (req, res) => {
   let books
+  let movies
   books = await Book.find().sort({ createAt: 'desc'}).limit(10).exec()
-  res.render('index.ejs', {books: books})
+  movies = await Movie.find().sort({ createAt: 'desc'}).limit(10).exec()
+  res.render('index.ejs', {books: books, movies: movies})
 })
 
 module.exports = router
